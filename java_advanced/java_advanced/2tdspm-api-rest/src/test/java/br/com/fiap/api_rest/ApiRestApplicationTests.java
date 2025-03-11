aea30931e0ef964a8175dc9ea0d9712bf3cc5882
irissuu <ykxiris@gmail.com>
2025-03-11 11:59:09 -0300

    swagger

--- java_advanced/java_advanced/2tdspm-api-rest/src/test/java/br/com/fiap/api_rest/ApiRestApplicationTests.java
+++ java_advanced/java_advanced/2tdspm-api-rest/src/test/java/br/com/fiap/api_rest/ApiRestApplicationTests.java
@@ -1,5 +1,7 @@
 package br.com.fiap.api_rest;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
